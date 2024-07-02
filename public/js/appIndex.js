14220f8c0c6c237fb0c292530ff701f11f340611
Sebastian <zzevazzz_1@hotmail.com>
2024-07-02 10:29:56 -0300

    finalAct_node_default_11

--- public/js/appIndex.js
+++ public/js/appIndex.js
@@ -225,7 +225,7 @@ addProduct.addEventListener('submit',async(e)=>{
 
      
        
-        const resp = await fetch("https://proyecto-production-7bcc.up.railway.app/products",{
+        const resp = await fetch("/products",{
             method: 'POST',
             body:formData
         })
