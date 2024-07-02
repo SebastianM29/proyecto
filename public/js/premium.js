d7ba7d75bdf834ba2c1b2bcd6a92f922e3cc91a7
Sebastian <zzevazzz_1@hotmail.com>
2024-07-02 11:19:15 -0300

    finalAct_node_default_11

--- public/js/premium.js
+++ public/js/premium.js
@@ -17,7 +17,7 @@ documentsPremium.addEventListener('submit', async(e) => {
     formData.append('document',documento)
     formData.append('home',domicilio)
   
-    const res = await fetch(`/api/session/users/premium/${id}/documents`,{
+    const res = await fetch(`https://proyecto-production-7bcc.up.railway.app/api/session/users/premium/${id}/documents`,{
       method: 'POST',
       body: formData
     })
@@ -49,7 +49,7 @@ const checkPremium = async() => {
     const id = document.querySelector('.namePerfil').getAttribute('data-id')
 
     console.log('hola');
-    const resp = await fetch(`/api/session/users/premium/${id}`, {
+    const resp = await fetch(`https://proyecto-production-7bcc.up.railway.app/api/session/users/premium/${id}`, {
       method: 'POST', 
       headers: {
           'Content-Type': 'application/json'
@@ -98,7 +98,7 @@ product.addEventListener('submit',async(e)=>{
 
    
       console.log(formData)
-      const resp = await fetch('/products',{
+      const resp = await fetch('https://proyecto-production-7bcc.up.railway.app/products',{
           method: 'POST',
           body:formData
       })
