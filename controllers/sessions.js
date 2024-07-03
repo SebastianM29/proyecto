274093a640cc9719a1a70d994bb9ec9f6d6a8727
Sebastian <zzevazzz_1@hotmail.com>
2024-07-03 11:32:27 -0300

    finalAct_node_default_10

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -252,7 +252,7 @@ export const documentPremium = async(req,res,next) => {
 
 export const picture= async (req,res) => {
 try {
-    console.log('se ve esto ===', req.file )
+    // console.log('se ve esto ===', req.file )
     console.log(req.body);
 
     const id = req.params.id
@@ -262,13 +262,13 @@ try {
 
 
    
-    if (picturepath !== 'empty') {
-        console.log('debo del el picturePath',picturepath);
-        const filePath = path.join(__dirname, '../public', picturepath);
-        console.log('Ruta completa a eliminar:', filePath);
-        await fs.unlink(filePath)
+    // if (picturepath !== 'empty') {
+    //     console.log('debo del el picturePath',picturepath);
+    //     const filePath = path.join(__dirname, '../public', picturepath);
+    //     console.log('Ruta completa a eliminar:', filePath);
+    //     await fs.unlink(filePath)
         
-    }
+    // }
    
     // const perfilPicture = 'https://proyecto-production-7bcc.up.railway.app/perfil/' + img
     // const actPicture = await User.findByIdAndUpdate(id,{perfilPicture:perfilPicture},{new:true})
