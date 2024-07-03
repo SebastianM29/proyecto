b4e0eac52763fd4df55920e37a86a056979e49a1
Sebastian <zzevazzz_1@hotmail.com>
2024-07-03 12:37:33 -0300

    finalAct_node_default_10

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -253,32 +253,32 @@ export const documentPremium = async(req,res,next) => {
 export const picture= async (req,res) => {
 try {
     console.log('hola llego bien');
-    // console.log('se ve esto ===', req.file )
-    // console.log(req.body);
+    console.log('se ve esto ===', req.file )
+    console.log(req.body);
 
-    // const id = req.params.id
-    // const img = req.file.filename
-    // const {picturepath} = req.body
-    // console.log(id);
+    const id = req.params.id
+    const img = req.file.filename
+    const {picturepath} = req.body
+    console.log(id);
 
 
    
-    // if (picturepath !== 'empty') {
-    //     console.log('debo del el picturePath',picturepath);
-    //     const filePath = path.join(__dirname, '../public', picturepath);
-    //     console.log('Ruta completa a eliminar:', filePath);
-    //     await fs.unlink(filePath)
+    if (picturepath !== 'empty') {
+        console.log('debo del el picturePath',picturepath);
+        const filePath = path.join(__dirname, '../public', picturepath);
+        console.log('Ruta completa a eliminar:', filePath);
+        await fs.unlink(filePath)
         
-    // }
+    }
    
-    // const perfilPicture = 'https://proyecto-production-7bcc.up.railway.app/perfil/' + img
-    // const actPicture = await User.findByIdAndUpdate(id,{perfilPicture:perfilPicture},{new:true})
-    // console.log('debo ver la carga',actPicture);
-    // req.session.user.perfilPicture = perfilPicture
-    // req.session.save()
-    // res.json({
-    //     msg: "enviado"
-    // })
+    const perfilPicture = 'https://proyecto-production-7bcc.up.railway.app/perfil/' + img
+    const actPicture = await User.findByIdAndUpdate(id,{perfilPicture:perfilPicture},{new:true})
+    console.log('debo ver la carga',actPicture);
+    req.session.user.perfilPicture = perfilPicture
+    req.session.save()
+    res.json({
+        msg: "enviado"
+    })
     
 
 } catch (error) {
