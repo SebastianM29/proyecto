2571bcdf9f05d0b5bbdf8c5a4aa89a066f4cfda6
Sebastian <zzevazzz_1@hotmail.com>
2024-07-04 13:28:59 -0300

    finalAct_node_default_18

--- dao/DTOs/user.dto.js
+++ dao/DTOs/user.dto.js
@@ -9,8 +9,9 @@ export default class UserDTO {
         this.carts = user.carts
         this.role = user.role || 'user'
         this.id = user.id || 'ID'
+        this.perfilPicture = user.perfilPicture
         // this.perfilPicture = user.perfilPicture || 'empty'
-        this.perfilPicture = user.perfilPicture !== undefined ? user.perfilPicture : 'empty';
+        // this.perfilPicture = user.perfilPicture !== undefined ? user.perfilPicture : 'empty';
         
         console.log('deberia ver el usuario: DTO',user)
     }
