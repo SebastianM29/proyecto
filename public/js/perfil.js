d361fb5f445415ba9e91191e21755e727bdfdb90
Sebastian <zzevazzz_1@hotmail.com>
2024-07-04 15:21:34 -0300

    finalAct_node_default_18

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -26,7 +26,7 @@ perfil.addEventListener('submit',async(e) =>{
         }
 
 
-        console.log('andando bien',picturepath);
+        console.log('andando bien',pictureElement);
         console.log('andando bien',pic);
         const resp = await fetch(`https://proyecto-production-1d58.up.railway.app/api/session/${id}/picture`,{
             method: 'POST',
