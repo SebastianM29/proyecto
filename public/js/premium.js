dc9ac9025e7ec757a2b8b484ec09469283a4a5fd
Sebastian <zzevazzz_1@hotmail.com>
2024-07-05 20:47:32 -0300

    commit_20

--- public/js/premium.js
+++ public/js/premium.js
@@ -23,7 +23,7 @@ const product = document.getElementById('addProduct')
         formData.append('domicilio',domicilio)
 
 
-       const url = `/api/session/users/premium/${id}/documents`
+       const url = `https://proyecto-production-1d58.up.railway.app/api/session/users/premium/${id}/documents`
         const res = await fetch(url,{
           method: 'POST',
           body: formData
