409ddee8cf366e8fcfc75b508c4d9cfbeb05b965
Sebastian <zzevazzz_1@hotmail.com>
2024-07-05 20:26:59 -0300

    commit_20

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -231,19 +231,19 @@ export const documentPremium = async(req,res,next) => {
         const id = req.params.uid
         const document = req.files.documento
         const home = req.files.domicilio
-        console.log('ver doument',document);
-        console.log(req.files);
-        const validatePremium={
-            document,
-            home
-        }
+        console.log('ver doument',document,home);
+    //     console.log(req.files);
+    //     const validatePremium={
+    //         document,
+    //         home
+    //     }
 
-       const user = await getUserByIdServAndCharge(id,validatePremium)
-       res.json(user)
+    //    const user = await getUserByIdServAndCharge(id,validatePremium)
+    //    res.json(user)
 
 
         
-        console.log(id);
+    //     console.log(id);
     } catch (error) {
         req.logger.error('Error en el update a Premium')
         console.log(error.message);
