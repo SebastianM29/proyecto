722708388e233251a3f35a8494afccd66e181c19
Sebastian <zzevazzz_1@hotmail.com>
2024-07-05 12:19:06 -0300

    finalAct_node_default_18

--- routes/session.routes.js
+++ routes/session.routes.js
@@ -65,7 +65,7 @@ router.get('/api/test/user', testUser )
 
 
 router.post('/:id/picture',uploadPicture.single('perfilPicture'),picture)
-router.post('/users/premium/:uid/documents',uploadDoc.fields([{name:'documento'},{name:'domicilio'}]),documentPremium)//va a ir actualizando a premium, subiendo los documentos que pide
+router.post('/users/premium/:uid/documents',uploadDoc.fields([{name: 'documento', maxCount: 1 },{name: 'domicilio', maxCount: 1 }]),documentPremium)//va a ir actualizando a premium, subiendo los documentos que pide
 router.post('/users/premium/:uid',premium)//deberia validar si el id q pasa es premium, si es premium poner "premium"
 
 
