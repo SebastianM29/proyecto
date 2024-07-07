86839a8f26719f5ffaada19a1bacddf6abaafc10
Sebastian <zzevazzz_1@hotmail.com>
2024-07-07 02:30:13 -0300

    commit_20

--- helpers/multer.js
+++ helpers/multer.js
@@ -8,7 +8,7 @@ const _dirname = dirname(_filename)
 
 export const storageDocuments = multer.diskStorage({
     destination: function(req,res,cb) {
-        cb(null,path.join(_dirname, '/.././public/documents'))
+        cb(null,path.join(_dirname, '/../public/documents'))
     },
     filename: function(req,file,cb) {
         cb(null,new Date().getTime() + path.extname(file.originalname))
